e6ad7d276d72a6fcd572a629ba20f48dc2c74dd7
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-18 01:37:42 -0600

    Index company names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120618072229) do
+ActiveRecord::Schema.define(:version => 20120618073535) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120618072229) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "companies", ["name"], :name => "index_companies_on_name", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "name"
