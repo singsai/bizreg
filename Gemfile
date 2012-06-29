fe1ff2a65d84c0c198a68a398a61268020945e77
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-28 18:55:13 -0600

    New layout/text changes for rebranding

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.1'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
 gem "dynamic_form"
+gem "twitter-bootstrap-rails"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,23 +2,23 @@ class UserMailer < ActionMailer::Base
   default :from => "info@palmling.com"
   
   def welcome_email(user, company)
-    mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for registering for PalmLing!", 
+    mail(:to => user.email, :bcc => "info@verbalizeit.com", :subject => "Thank you for registering for VerbalizeIt!", 
     :content_type => "text/html", :body => "Dear #{user.name}, <br/><br/>Thank you for registering with 
-    <a href='http://palmling.com'>PalmLing</a>. 
-    PalmLing delivers real-time access to a community of human translators to facilitate cross language conversations. 
+    <a href='http://verbalizeit.com'>VerbalizeIt</a>. 
+    VerbalizeIt delivers real-time access to a community of human translators to facilitate cross language conversations. 
     As a valued member of #{company.name}, you will have free access to our Spanish<->English translation service for 5 days.
     <br/><br/>
     After 5 days, this phone number will expire and you can register for a premium account by visiting
-    <a href='http://palmling.com'>PalmLing.com</a>
+    <a href='http://verbalizeit.com'>VerbalizeIt.com</a>
     <br/><br/>
     To access our global community of translators, call #{company.phone_number}. 
     To be considered for promotions, discounts and other prizes, please make sure your email has been registered at
     <a href='#{HOST_URL}/companies/#{company.permalink}/register'>'#{HOST_URL}/companies/#{company.permalink}/register'</a>.
     <br/><br/>Thank you, <br/><br/>
-    The PalmLing Team, <br/><a href='http://www.palmling.com'>PalmLing<a/>. <br/><br/>
-    <a href='https://twitter.com/#!/palmling'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/palmling'>Twitter</a><br/>
-    <a href='http://www.facebook.com/pages/PalmLing/108198825934073'>
-    <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/pages/PalmLing/108198825934073'>Facebook</a>")    
+    The VerbalizeIt Team, <br/><a href='http://www.verbalizeit.com'>VerbalizeIt<a/>. <br/><br/>
+    <a href='https://twitter.com/#!/verbalize_it'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/verbalize_it'>Twitter</a><br/>
+    <a href='http://www.facebook.com/verbalizeit'>
+    <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/verbalizeit'>Facebook</a>")    
   end
 
   # def test_email(user)
